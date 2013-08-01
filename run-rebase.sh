9567a8b00b73375e84d3e477c855109ac5ca0ded
John Morris <john@zultron.com>
2013-08-01 18:00:24 -0500

    automatic edits & author fixes; testing mode
    Add automatic edits: Patches with the short-sha1 can be placed in the
    patches directory and they will be automatically committed at an
    'edit' command instead of the EDITOR being invoked
    
    Add automatic author fixes: Like above, author & email may be placed
    in a file; this will be overridden at an 'edit' command.
    
    Testing mode: separate $upstream and $orig_head, and separate todo
    list.

--- run-rebase.sh
+++ run-rebase.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 #
 # Run git rebase --interactive, but without interaction
 
@@ -6,6 +6,7 @@
 # git-rebase--interactive script, and set the below settings.
 
 # Options:
+# -t:  run in testing mode (if present, this must be first)
 # -l:  print a graph of $upstream..HEAD
 # -L:  print a graph of $upstream..$orig_head
 # -e:  manually edit the todo list instead of using rebase-script.txt
@@ -13,19 +14,32 @@
 #############################################################
 # settings
 
-# set branch and upstream commits
-orig_head=linuxcnc/unified-build-candidate-0
-#orig_head=rtos-integration-preview3-clean-v2.5_branch-merge-ub
-upstream=9d7fbdd
+if ! test "$1" = -t; then
+    # set branch and upstream commits
+    upstream=9d7fbdd
+    orig_head=linuxcnc/unified-build-candidate-0
 
-# a name for the new branch
-new_branch=ubc0-rebase
+    # a name for the new branch
+    new_branch=ubc0-rebase
+
+else
+    # testing:  set branch and upstream commits
+    upstream=ce95062
+    orig_head=df1261d
+    TESTING=true; shift
+
+    # a name for the testing branch
+    new_branch=run-rebase-testing
+
+fi
 
 # pretty format for git log --graph
 pretty='--pretty=format:"%h [%an] %s"'
 
 #############################################################
 # set up variables
+
+test "$TESTING" = true || TESTING=false
 
 # for 'git log' output to include the parent commit but not
 # grandparents
@@ -44,19 +58,26 @@ if test "$1" != -e; then
     export EDITOR="$REBASE_HOME/cp-file.sh"
 
     # The interactive rebase script, used by cp-todo.sh
-    export REBASE_TODO="$REBASE_HOME/rebase-script.txt"
+    if ! $TESTING; then
+	export REBASE_TODO="$REBASE_HOME/rebase-script.txt"
+    else
+	export REBASE_TODO="$REBASE_HOME/rebase-script-testing.txt"
+    fi
 else
     # remove -e from args
     shift
 fi
 
-excludes="$(echo $GIT_REBASE_EXCLUDE | \
-	awk '{ l = "";for (i=1;i<=NF;i++) l=l " ^" $i;print l}')"
-
 # Exclude any commits from master
 # This removes commits that don't need to be rebased
 export GIT_REBASE_EXCLUDE=master
 
+excludes="$(echo $GIT_REBASE_EXCLUDE | \
+	awk '{ l = "";for (i=1;i<=NF;i++) l=l " ^" $i;print l}')"
+
+# Tell edit command to look for patches/<sha1>.patch to apply instead
+# of exiting for user to manually edit
+export GIT_REBASE_PATCHES=$REBASE_HOME/patches
 
 #############################################################
 # run the rebase
@@ -67,20 +88,21 @@ rebase() {
     rm -fr ".git/rebase-merge"
 
     # git rid of any old rebase branch
-    git branch -M $new_branch $new_branch-$$ 2>/dev/null && moved_branch=true
+    git branch -M $new_branch $new_branch-$$ 2>/dev/null \
+	&& moved_branch=true || moved_branch=false
 
     # exit if the following fail
     set -e
 
     # check out a clean copy of the branch to rebase
-    git checkout -b $new_branch $orig_head 2>/dev/null && git reset --hard
+    git reset --hard
+    git checkout -b $new_branch $orig_head
 
     # remove old rebase branch
     $moved_branch && git branch -D $new_branch-$$ || true
 
     # execute the rebase
     git rebase -i -p $upstream
-
     # now let the user sort out all the pieces.  :P
 }
 
